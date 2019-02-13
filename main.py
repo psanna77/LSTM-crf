1dfb521b5688143ecf0feba69f12a27609a926d5
psanna77 <poudyal@seas.upenn.edu>
2019-02-13 14:44:50 -0500

    change to char level

--- main.py
+++ main.py
@@ -62,15 +62,15 @@ class LSTMTagger(nn.Module):
 ##################### Trainer #####################
 if __name__ == '__main__':
     training_data = [
-    ("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
-    ("Everybody read that book".split(), ["NN", "V", "DET", "NN"])
+    (list("The dog ate the apple"), ["DET", "NN", "V", "DET", "NN"]),
+    (list("Everybody read that book"), ["NN", "V", "DET", "NN"])
     ]
-    word_to_ix = {}
-    for sent, tags in training_data:
-        for word in sent:
-            if word not in word_to_ix:
-                word_to_ix[word] = len(word_to_ix)
-    print(word_to_ix)
+    char_to_ix = {}
+    for chars, tags in training_data:
+        for ch in chars:
+            if ch not in char_to_ix:
+                char_to_ix[ch] = len(char_to_ix)
+    print(char_to_ix)
     tag_to_ix = {"DET": 0, "NN": 1, "V": 2}
 
     # These will usually be more like 32 or 64 dimensional.
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     EMBEDDING_DIM = 6
     HIDDEN_DIM = 6
 
-    model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix))
+    model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(char_to_ix), len(tag_to_ix))
     loss_function = nn.NLLLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.1)
 
@@ -86,12 +86,12 @@ if __name__ == '__main__':
     # Note that element i,j of the output is the score for tag j for word i.
     # Here we don't need to train, so the code is wrapped in torch.no_grad()
     with torch.no_grad():
-        inputs = prepare_sequence(training_data[0][0], word_to_ix)
+        inputs = prepare_sequence(training_data[0][0], char_to_ix)
         tag_scores = model(inputs)
         print(tag_scores)
 
     for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
-        for sentence, tags in training_data:
+        for char_list, tags in training_data:
             # Step 1. Remember that Pytorch accumulates gradients.
             # We need to clear them out before each instance
             model.zero_grad()
@@ -102,11 +102,11 @@ if __name__ == '__main__':
 
             # Step 2. Get our inputs ready for the network, that is, turn them into
             # Tensors of word indices.
-            sentence_in = prepare_sequence(sentence, word_to_ix)
+            char_list_in = prepare_sequence(char_list, char_to_ix)
             targets = prepare_sequence(tags, tag_to_ix)
 
             # Step 3. Run our forward pass.
-            tag_scores = model(sentence_in)
+            tag_scores = model(char_list_in)
 
             # Step 4. Compute the loss, gradients, and update the parameters by
             #  calling optimizer.step()
@@ -116,7 +116,7 @@ if __name__ == '__main__':
 
     # See what the scores are after training
     with torch.no_grad():
-        inputs = prepare_sequence(training_data[0][0], word_to_ix)
+        inputs = prepare_sequence(training_data[0][0], char_to_ix)
         tag_scores = model(inputs)
 
         # The sentence is "the dog ate the apple".  i,j corresponds to score for tag j
